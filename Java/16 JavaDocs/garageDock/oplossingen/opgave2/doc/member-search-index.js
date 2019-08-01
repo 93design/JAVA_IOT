7e85e7b496af08b211eb011b7447ab634872e87d
93design <goegebeurjamie@gmail.com>
2019-08-01 11:39:25 +0200

    adding links

--- Java/16 JavaDocs/garageDock/oplossingen/opgave2/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"app","c":"GarageAppSmallRandom","l":"GarageAppSmallRandom()","url":"%3Cinit%3E()"},{"p":"app","c":"GarageAppSmallRandom","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"app","c":"GarageAppSmallRandom","l":"MAX_SPEED"}]
